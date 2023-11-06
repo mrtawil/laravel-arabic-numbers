db1f4fb8ffc40d0f6c6b723ec6ca38de3007c7fa
Abed El Rahman Al Tawil <63475475+mrtawil@users.noreply.github.com>
2023-11-06 19:32:19 +0200

    Update ConvertArabicDigitsToEnlishMiddleware.php
    Check if value is null or doesn't contain Hindi numbers

--- src/Http/Middleware/ConvertArabicDigitsToEnlishMiddleware.php
+++ src/Http/Middleware/ConvertArabicDigitsToEnlishMiddleware.php
@@ -2,17 +2,20 @@
 
 namespace Alkoumi\LaravelArabicNumbers\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+use Illuminate\Foundation\Http\Middleware\TransformsRequest as Middleware;
 
-class ConvertArabicDigitsToEnlishMiddleware extends TransformsRequest
+class ConvertArabicDigitsToEnglishMiddleware extends Middleware
 {
+    protected const HINDI_NUMBERS = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+    protected const ARABIC_NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
     /**
      * The attributes that should not be transformed.
      *
      * @var array
      */
     protected $except = [
-        'password', 'password_confirmation' //
+        'password', 'password_confirmation', 'password_current',
     ];
 
     /**
@@ -24,16 +27,34 @@ class ConvertArabicDigitsToEnlishMiddleware extends TransformsRequest
      */
     protected function transform($key, $value)
     {
+        if (!$value) {
+            return $value;
+        }
+
         if (in_array($key, $this->except, true)) {
             return $value;
         }
+
+        if (!$this->containsHindiNumbers($value)) {
+            return $value;
+        }
+
         return $this->transNumber($value);
+    }
+
+    protected function containsHindiNumbers($value)
+    {
+        foreach (Self::HINDI_NUMBERS as $number) {
+            if (strpos($value, $number) !== false) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     protected function transNumber($value)
     {
-        $hindi_numbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
-        $arabic_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        return str_replace($hindi_numbers, $arabic_numbers, $value);
+        return str_replace(Self::HINDI_NUMBERS, Self::ARABIC_NUMBERS, $value);
     }
 }
